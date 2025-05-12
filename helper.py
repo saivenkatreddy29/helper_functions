71b7f931a1cd5a77b194146fb9e92948f7c56a8a
Sai venkat reddy sheri <85113716+saivenkatreddy29@users.noreply.github.com>
2025-05-12 14:52:13 -0400

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,1 +1,9 @@
+import os
 
+def get_image_full_directory(directory):
+    images_list = os.listdir(image_path)
+    images_full_list =[]
+    for image in images_list:
+        im_f = os.path.join(directory,image)
+        images_full_list.append(im_f)
+    return images_full_list
